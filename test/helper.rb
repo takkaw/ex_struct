a40f098f2b5f341d7b04045af404a63d6748ddd3
takkaw <takkaw@gmail.com>
2011-08-09 10:45:48 +0900

    add test

--- test/helper.rb
+++ test/helper.rb
@@ -4,6 +4,7 @@ require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'minifilter/pretty' if Gem.available? 'minifilter'
 require 'ex_struct'
 
 class Test::Unit::TestCase
--- test/test_ex_struct.rb
+++ test/test_ex_struct.rb
@@ -1,7 +1,37 @@
 require 'helper'
 
 class TestExStruct < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  should "add new method as value = 100" do
+    conf = ExStruct.new
+    conf.value = 100
+    conf.foo.value = 200
+    assert_equal conf.value,100
+  end
+  should "touch new method" do
+    conf = ExStruct.new
+    conf.test
+    assert ExStruct === conf.test
+  end
+  should "add new multiple method" do
+    conf = ExStruct.new
+    conf.foo.bar.baz = "ok"
+    assert_equal conf.foo.bar.baz ,"ok"
+  end
+  should "not yet touch method is nil" do
+    conf = ExStruct.new
+    assert_equal conf.not_yet? , nil
+  end
+  should "raise ArgumentError" do
+    conf = ExStruct.new
+    assert_raise(ArgumentError) {
+      conf[1,2,3] = :test
+    }
+    assert_raise(ArgumentError) {
+      conf.test? :foo,:bar
+    }
+    assert_raise(ArgumentError){
+      conf.test :foo
+    }
   end
 end
+
